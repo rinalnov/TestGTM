32557d16fbbb8435983ba61bd8fd3f6abe1d8e6e
RinalNov <>
2018-11-22 11:06:38 +0900

    add firebase

---
+++ Pods/Target Support Files/Pods-TestGTM/Pods-TestGTM-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TestGTM : NSObject
+@end
+@implementation PodsDummy_Pods_TestGTM
+@end
---
+++ TestGTM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestGTM
+//
+//  Created by RinalNov on 11/22/18.
+//  Copyright © 2018 RinalNov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
